341bd30edc77864f7729da5b785a741075157d28
DIGITAL <scholarann2020@gmail.com>
2025-10-28 14:17:36 +0500

    Code Added class is created with function

---
+++ Odd and Even.py
@@ -0,0 +1,8 @@
+print("Welcome to the project that identifies odd and even numbers")
+
+n = int(input("Enter the number to find if it is odd or even: "))
+
+if n % 2 == 0:
+    print('Even')
+else:
+    print('Odd')
